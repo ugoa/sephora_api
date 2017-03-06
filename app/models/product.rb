832de69df2b4dfb79e0a7df231c247fedca87e74
David.H <hoodavy@gmail.com>
2017-03-06 18:29:22 +0800

    model specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true
 
-  validates :category, inclusion: { in: %w(markup tools brushes) }
+  validates :category,
+    inclusion: { in: %w(markup tools brushes) },
+    allow_nil: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  
+  it { is_expected.to validate_presence_of(:name) }
+  it do
+    is_expected.to validate_inclusion_of(:category)
+      .in_array(%w(markup tools brushes))
+  end
+
 end
