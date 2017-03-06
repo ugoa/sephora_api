642caff7232c22ff17c6ed1496e5375c236258aa
David.H <hoodavy@gmail.com>
2017-03-06 16:31:27 +0800

    product table fields

--- db/migrate/20170306082512_create_products.rb
+++ db/migrate/20170306082512_create_products.rb
@@ -1,6 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.string :name, index: true
+      t.boolean :sold_out
+      t.string :category, index: true
+      t.integer :price
+      t.boolean :under_sale
+      t.integer :sale_price
 
       t.timestamps
     end
